7a769de14f3da7e1c744947ed271c7c3a377eacf
DGamers-64 <dgamers.643@gmail.com>
2026-03-23 22:14:57 +0100

    Commit inicial

---
+++ index.js
@@ -0,0 +1,33 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export default class LuneDataBase {
+    constructor(path) {
+        this.path = path;
+    }
+
+    async get() {
+        const rawData = await readFile(this.path, 'utf-8');
+        const datos = JSON.parse(rawData);
+        return datos;
+    }
+
+    async set(nuevosDatos) {
+        const datosString = JSON.stringify(nuevosDatos, null, 2);
+        await writeFile(this.path, datosString, 'utf-8');
+        return datosString;
+    }
+
+    async add(nuevosDatos) {
+        const datosExistentes = await this.get();
+
+        let datosActualizados;
+        if (Array.isArray(datosExistentes)) {
+            datosActualizados = [...datosExistentes, ...nuevosDatos];
+        } else {
+            datosActualizados = { ...datosExistentes, ...nuevosDatos };
+        }
+
+        await writeFile(this.path, JSON.stringify(datosActualizados, null, 2), 'utf-8');
+        return datosActualizados;
+    }
+}
